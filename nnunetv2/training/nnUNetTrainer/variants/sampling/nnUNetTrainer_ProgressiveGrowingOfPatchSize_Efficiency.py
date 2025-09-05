8f7437eb65f36d1a3b17cd341c070b31fc320962
Stefan Fischer <stefan.mi.fischer@tum.de>
2025-09-05 14:30:11 +0200

    update convert seg to regions transform

--- nnunetv2/training/nnUNetTrainer/variants/sampling/nnUNetTrainer_ProgressiveGrowingOfPatchSize_Efficiency.py
+++ nnunetv2/training/nnUNetTrainer/variants/sampling/nnUNetTrainer_ProgressiveGrowingOfPatchSize_Efficiency.py
@@ -109,6 +109,31 @@ def _verify_spatial_size(size: list[int]) -> None:
 
 F._verify_spatial_size = _verify_spatial_size
 
+class ConvertSegmentationToRegionsTransform2(AbstractTransform):
+    def __init__(self, regions: Union[List, Tuple],
+                 seg_key: str = "seg", output_key: str = "seg", seg_channel: int = 0):
+        """
+        regions are tuple of tuples where each inner tuple holds the class indices that are merged into one region,
+        example:
+        regions= ((1, 2), (2, )) will result in 2 regions: one covering the region of labels 1&2 and the other just 2
+        :param regions:
+        :param seg_key:
+        :param output_key:
+        """
+        self.seg_channel = seg_channel
+        self.output_key = output_key
+        self.seg_key = seg_key
+        self.regions = regions
+    def __call__(self, **data_dict):
+        seg = data_dict.get(self.seg_key)
+        if seg is not None:
+            b, c, *shape = seg.shape
+            region_output = np.zeros((b, len(self.regions), *shape), dtype=bool)
+            for region_id, region_labels in enumerate(self.regions):
+                region_output[:, region_id] |= np.isin(seg[:, self.seg_channel], region_labels)
+            data_dict[self.output_key] = region_output.astype(np.uint8, copy=False)
+        return data_dict
+
 
 
 class DownsampleSegForDSTransform3(AbstractTransform):
@@ -503,7 +528,7 @@ class nnUNetTrainer_ProgressiveGrowingOfPatchSize_Efficiency(nnUNetTrainer):
 
         if regions is not None:
             # the ignore label must also be converted
-            val_transforms.append(ConvertSegmentationToRegionsTransform(list(regions) + [ignore_label]
+            val_transforms.append(ConvertSegmentationToRegionsTransform2(list(regions) + [ignore_label]
                                                                         if ignore_label is not None else regions,
                                                                         'target', 'target'))
 
@@ -602,7 +627,7 @@ class nnUNetTrainer_ProgressiveGrowingOfPatchSize_Efficiency(nnUNetTrainer):
 
         if regions is not None:
             # the ignore label must also be converted
-            tr_transforms.append(ConvertSegmentationToRegionsTransform(list(regions) + [ignore_label]
+            tr_transforms.append(ConvertSegmentationToRegionsTransform2(list(regions) + [ignore_label]
                                                                        if ignore_label is not None else regions,
                                                                        'target', 'target'))
 
